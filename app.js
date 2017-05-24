670ed3774b6bdad6509f97d982b6d89ac477b49c
fliermanrw <r.w.flierman@st.hanze.nl>
2017-05-24 11:09:00 +0200

    Bijwerken

--- app.js
+++ app.js
@@ -6,58 +6,72 @@ app.controller('PosController', function ($scope) {
     $scope.dranken = [{
         id: 0,
         name: "Spa Blauw",
-        price: "1",
+        price: "1"
     },
     {
         id: 1,
         name: "Spa Rood",
-        price: "1.10",
+        price: "1.10"
     },
     {
         id: 2,
         name: "Espresso",
-        price: "1.20",
+        price: "1.20"
     },
     {
         id: 3,
         name: "Cappuccino",
-        price: "1.30",
+        price: "1.30"
     },
     {
         id: 4,
         name: "Thee",
-        price: "1.90",
+        price: "1.90"
     },
     {
         id: 5,
         name: "Warme chocolademelk",
-        price: "2.10",
+        price: "2.10"
     },
     {
         id: 6,
         name: "Cola",
-        price: "2.00",
+        price: "2.00"
     },
     {
         id: 7,
         name: "Sinaasappelsap",
-        price: "1.90",
+        price: "1.90"
     }];
 
     $scope.eten = [{
         id: 8,
         name: "Tosti",
-        price: "1.50",
+        price: "1.50"
     },
     {
         id: 9,
         name: "Taart",
-        price: "1.30",
+        price: "1.30"
     },
     {
         id: 10,
         name: "Nacho's",
-        price: "1.70",
+        price: "1.70"
+    }];
+
+    $scope.tables = [{
+        id: 11,
+        name: "Tafel 1"
+        },
+        {
+            id: 12,
+            name: "Tafel 2"
+        },
+        {
+        id: 13,
+        name: "Tafel 3"
+
     }];
 
     $scope.order = [];
@@ -130,22 +144,31 @@ app.controller('PosController', function ($scope) {
         $scope.order = [];
     };
 
+    $scope.addToTable = function () {
+        alert($scope.getDate() + " - Ordernummer: " + ($scope.totOrders+1) + "\n\n Toegevoegd aan tafel!");
+        $scope.totOrders += 1;
+    };
+
     $scope.checkout = function (index) {
         alert($scope.getDate() + " - Ordernummer: " + ($scope.totOrders+1) + "\n\nTotaalbedrag: €" + $scope.getTotal().toFixed(2) + "\n\nBetaling ontvangen. Bedankt!");
         $scope.order = [];
         $scope.totOrders += 1;
     };
 
+    $scope.selectTable = function(table){
+        //TODO
+    };
+
     $scope.addNewItem = function (item) {
         if (item.category === "Dranken") {
-            item.id = $scope.dranken.length + $scope.eten.length
-            $scope.dranken.push(item)
-            $scope.nieuw = []
+            item.id = $scope.dranken.length + $scope.eten.length;
+            $scope.dranken.push(item);
+            $scope.nieuw = [];
             $('#myTab').find('a[href="#dranken"]').tab('show')
         } else if (item.category === "Eten") {
-            item.id = $scope.eten.length + $scope.eten.length
-            $scope.eten.push(item)
-            $scope.nieuw = []
+            item.id = $scope.eten.length + $scope.eten.length;
+            $scope.eten.push(item);
+            $scope.nieuw = [];
             $('#myTab').find('a[href="#eten"]').tab('show')
         }
     };
