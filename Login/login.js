d4b200f3fe94887f43f035b35df2fc2ae4f23ffb
MartijnSnijder <martijn-snijder@hotmail.com>
2017-05-29 12:14:43 +0200

    Refactored and added login files

--- Login/login.js
+++ Login/login.js
@@ -1,4 +1,36 @@
 /**
  * Created by Martijn on 29-5-2017.
  */
-//todo create login handling
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
+    function LoginController($location, AuthenticationService, FlashService) {
+        var vm = this;
+
+        vm.login = login;
+
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            vm.dataLoading = true;
+            AuthenticationService.Login(vm.username, vm.password, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(vm.username, vm.password);
+                    $location.path('/');
+                } else {
+                    FlashService.Error(response.message);
+                    vm.dataLoading = false;
+                }
+            });
+        };
+    }
+
+})();
--- app.js
+++ app.js
@@ -1,204 +1,56 @@
-var app = angular.module('myApp', []);
+(function () {
+    'use strict';
 
-app.controller('PosController', function ($scope) {
+    angular
+        .module('App', ['ngRoute', 'ngCookies'])
+        .config(config)
+        .run(run);
 
-    $scope.favorieten = [{
-        id: 15,
-        name: "Rode wijn (glas)",
-        price: "4"
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'PosController',
+                templateUrl: 'Home/home.html',
+                controllerAs: 'vm'
+            })
 
-    }, {
-        id: 16,
-        name: "Rode wijn (fles)",
-        price: "14"
-    },{
-        id: 17,
-        name: "Koffie verkeerd",
-        price: "4,20"
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'Login/login.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/register', {
+                controller: 'RegisterController',
+                templateUrl: 'Register.register.html',
+                controllerAs: 'vm'
+            })
+            .when('/dashboard', {
+                controller: 'DashboardController',
+                templateUrl: 'Dashboard.dashboard.html',
+                controllerAs: 'vm'
+            })
+
+            .otherwise({ redirectTo: '/login' });
     }
 
-    ];
-
-    $scope.dranken = [{
-        id: 0,
-        name: "Cola",
-        price: "1.50"
-    },
-    {
-        id: 1,
-        name: "Sinas",
-        price: "1.50"
-    },
-    {
-        id: 2,
-        name: "Espresso",
-        price: "2.50"
-    },
-    {
-        id: 3,
-        name: "Cappuccino",
-        price: "2.50"
-    },
-    {
-        id: 4,
-        name: "Thee",
-        price: "1.90"
-    },
-    {
-        id: 5,
-        name: "Koffie",
-        price: "2.00"
-    },
-    {
-        id: 6,
-        name: "Bier",
-        price: "2.40"
-    },
-    {
-        id: 7,
-        name: "Radler",
-        price: "2.70"
-    }];
-
-    $scope.eten = [{
-        id: 8,
-        name: "Tosti",
-        price: "1.50"
-    },
-    {
-        id: 9,
-        name: "Taart",
-        price: "1.30"
-    },
-    {
-        id: 10,
-        name: "Nacho's",
-        price: "1.70"
-    }];
-
-    $scope.tafels = [{
-        id: 11,
-        name: "1"
-        },
-        {
-            id: 12,
-            name: "2"
-        },
-        {
-        id: 13,
-        name: "3"
-
-    }];
-
-    $scope.order = [];
-    $scope.nieuw = {};
-    $scope.totOrders = 0;
-    $scope.comments = {};
-
-    var url = window.location.protocol + "://" + window.location.host + "/" + window.location.pathname;
-
-    $scope.getDate = function () {
-        var today = new Date();
-        var mm = today.getMonth() + 1;
-        var dd = today.getDate();
-        var yyyy = today.getFullYear();
-
-        var date = dd + "/" + mm + "/" + yyyy
-
-        return date
-    };
-
-    $scope.addToOrder = function (item, qty) {
-        var flag = 0;
-        if ($scope.order.length > 0) {
-            for (var i = 0; i < $scope.order.length; i++) {
-                if (item.id === $scope.order[i].id) {
-                    item.qty += qty;
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag === 0) {
-                item.qty = 1;
-            }
-            if (item.qty < 2) {
-                $scope.order.push(item);
-            }
-        } else {
-            item.qty = qty;
-            $scope.order.push(item);
+    run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
+    function run($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
         }
-    };
 
-    $scope.removeOneEntity = function (item) {
-        for (var i = 0; i < $scope.order.length; i++) {
-            if (item.id === $scope.order[i].id) {
-                item.qty -= 1;
-                if (item.qty === 0) {
-                    $scope.order.splice(i, 1);
-                }
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/login');
             }
-        }
-    };
-
-    $scope.removeItem = function (item) {
-        for (var i = 0; i < $scope.order.length; i++) {
-            if (item.id === $scope.order[i].id) {
-                $scope.order.splice(i, 1);
-            }
-        }
-    };
-
-    $scope.getTotal = function () {
-        var tot = 0;
-        for (var i = 0; i < $scope.order.length; i++) {
-            tot += ($scope.order[i].price * $scope.order[i].qty)
-        }
-        return tot;
-    };
-
-    $scope.clearOrder = function () {
-        $scope.order = [];
-    };
-
-    $scope.addToTable = function () {
-        alert($scope.getDate() + " - Ordernummer: " + ($scope.totOrders+1) + "\n\n Toegevoegd aan tafel!");
-        $scope.order= [];
-        $scope.totOrders += 1;
-
-    };
-
-    $scope.checkout = function (index) {
-        alert($scope.getDate() + " - Ordernummer: " + ($scope.totOrders+1) + "\n\nTotaalbedrag: €" + $scope.getTotal().toFixed(2) + "\n\nBetaling ontvangen. Bedankt!");
-        $scope.order = [];
-        $scope.totOrders += 1;
-    };
-
-    $scope.selectTable = function(table){
-        //TODO
-    };
-
-    $scope.addNewItem = function (item) {
-        if (item.category === "Dranken") {
-            item.id = $scope.dranken.length + $scope.eten.length;
-            $scope.dranken.push(item);
-            $scope.nieuw = [];
-            $('#myTab').find('a[href="#dranken"]').tab('show')
-        } else if (item.category === "Eten") {
-            item.id = $scope.eten.length + $scope.eten.length;
-            $scope.eten.push(item);
-            $scope.nieuw = [];
-            $('#myTab').find('a[href="#eten"]').tab('show')
-        } else if(item.category === "favorieten"){
-            item.id = $scope.favorieten.length + $scope.eten.length;
-            $scope.favorieten.push(item);
-            $scope.nieuw = [];
-        }
-    };
-
-    $scope.submitComment = function (comment) {
-        $scope.comments.push(comment);
-        return $scope.comments;
+        });
     }
 
-});
+})();
