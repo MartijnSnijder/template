0a6e3a021003b8eef307e7c975d57286f5410b35
MartijnSnijder <martijn-snijder@hotmail.com>
2017-05-29 12:31:27 +0200

    updated index and app

--- app.js
+++ app.js
@@ -40,7 +40,7 @@
         // keep user logged in after page refresh
         $rootScope.globals = $cookieStore.get('globals') || {};
         if ($rootScope.globals.currentUser) {
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
         }
 
         $rootScope.$on('$locationChangeStart', function (event, next, current) {
