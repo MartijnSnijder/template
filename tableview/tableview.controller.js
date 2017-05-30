f57819ed62b0f0a89c2e6ea2014a4cabc50b9ead
MartijnSnijder <martijn-snijder@hotmail.com>
2017-05-30 21:53:10 +0200

    Refactored and fixed tableViewController

--- tableview/tableview.controller.js
+++ tableview/tableview.controller.js
@@ -2,122 +2,131 @@
  * Created by r_fli on 29-5-2017.
  */
 
-var app = angular.module('app', []);
+(function () {
+    'use strict';
 
-app.controller('tableViewController', function ($scope) {
+    angular
+        .module('app')
+        .controller('tableViewController', tableViewController);
 
-    $scope.tableSectionA = [{
-        id: 23,
-        name: "A-1",
-        order: []
+    tableViewController.$inject = ['$rootScope'];
+    function tableViewController($rootScope) {
+        var vm = this;
 
-    }, {
-        id: 21,
-        name: "A-2",
-        order: []
-    }, {
-        id: 22,
-        name: "A-3",
-        order: []
-    }, {
-        id: 22,
-        name: "A-4",
-        order: []
-    }, {
-        id: 22,
-        name: "A-5",
-        order: []
-    },{
-        id: 22,
-        name: "A-6",
-        order: []
-    }
+        // variable
+        $rootScope.show = true;
 
-    ];
-
-    $scope.tableSectionB = [{
-        id: 23,
-        name: "B-1",
-        order: [{
-            name: "koffie",
-            price: "2",
-            qty: "4"
-        },{
-            name:"thee",
-            price:"3",
-            qty: "8"
-        }
-
-        ]
-
-    }, {
-        id: 24,
-        name: "B-2",
-        order: [{
-            name: "BIER",
-            price: "2",
-            qty: "25"
-        }]
-    }, {
-        id: 25,
-        name: "B-3",
-        order: []
-    }, {
-        id: 24,
-        name: "B-4",
-        order: []
-    }, {
-        id: 25,
-        name: "B-5",
-        order: []
-    }
-
-    ];
-
-    $scope.test=function () {
-        return $scope.tableSectionB[1].price;
-    };
-
-
-    $scope.getTotal = function () {
-        var tot = 0;
-        for (var i = 0; i < $scope.tableSectionB.length; i++) {
-            tot += ($scope.tableSectionB[i].price * $scope.tableSectionB[i].qty)
-        }
-        return tot;
-    };
-
-
-
-    $scope.orderTemp = [{
+        $rootScope.orderTemp = [{
             id: 24,
             name: "Thee",
             price: "2",
             qty: "4"
 
         },
-        {
-            id:22,
-            name: "koffie",
-            price: "4",
-            qty: "1"
-    }];
-    
-    $scope.getTable = function (tableID) {
-        return tableID;
-    };
+            {
+                id:22,
+                name: "koffie",
+                price: "4",
+                qty: "1"
+            }];
 
-    // variable
-    $scope.show = true;
+        $rootScope.tableSectionA = [{
+            id: 23,
+            name: "A-1",
+            order: []
 
-    $scope.showHide = function(){
-        if(show = true){
-            show = false;
-        } else{
-            show = true;
+        }, {
+            id: 21,
+            name: "A-2",
+            order: []
+        }, {
+            id: 22,
+            name: "A-3",
+            order: []
+        }, {
+            id: 22,
+            name: "A-4",
+            order: []
+        }, {
+            id: 22,
+            name: "A-5",
+            order: []
+        },{
+            id: 22,
+            name: "A-6",
+            order: []
         }
 
+        ];
+
+        $rootScope.tableSectionB = [{
+            id: 23,
+            name: "B-1",
+            order: [{
+                name: "koffie",
+                price: "2",
+                qty: "4"
+            },{
+                name:"thee",
+                price:"3",
+                qty: "8"
+            }
+
+            ]
+
+        }, {
+            id: 24,
+            name: "B-2",
+            order: [{
+                name: "BIER",
+                price: "2",
+                qty: "25"
+            }]
+        }, {
+            id: 25,
+            name: "B-3",
+            order: []
+        }, {
+            id: 24,
+            name: "B-4",
+            order: []
+        }, {
+            id: 25,
+            name: "B-5",
+            order: []
+        }
+
+        ];
+
+        $rootScope.test = function () {
+            return $rootScope.tableSectionB[1].price;
+        };
+
+
+        $rootScope.getTotal = function () {
+            var tot = 0;
+            for (var i = 0; i < $rootScope.tableSectionB.length; i++) {
+                tot += ($rootScope.tableSectionB[i].price * $rootScope.tableSectionB[i].qty)
+            }
+            return tot;
+        };
+
+
+        $rootScope.getTable = function (tableID) {
+            return tableID;
+        };
+
+        $rootScope.showHide = function(){
+            if($rootScope.show = true){
+                $rootScope.show = false;
+            } else{
+                $rootScope.show = true;
+            }
+
+        };
+
+        function initController() {
+            // todo init
+        }
     }
-
-
-});
+})();
