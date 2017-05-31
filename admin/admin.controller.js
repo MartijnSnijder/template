93a4ee83ede74525035a09960d910cb70bd3ff54
MartijnSnijder <martijn-snijder@hotmail.com>
2017-05-31 13:41:34 +0200

    Added ability to remove users

--- admin/admin.controller.js
+++ admin/admin.controller.js
@@ -9,9 +9,16 @@
         .module('app')
         .controller('AdminController', AdminController);
 
-    AdminController.$inject = ['$rootScope'];
-    function AdminController($rootScope) {
+    AdminController.$inject = ['UserService', '$rootScope'];
+    function AdminController(UserService, $rootScope) {
+
         var vm = this;
+
+        vm.user = null;
+        vm.allUsers = [];
+        vm.deleteUser = deleteUser;
+
+        initController();
 
         $rootScope.addNewItem = function (item) {
             if (item.category === "Dranken") {
@@ -31,8 +38,31 @@
             }
         };
 
+
         function initController() {
-            // todo init
+            loadCurrentUser();
+            loadAllUsers();
+        }
+
+        function loadCurrentUser() {
+            UserService.GetByUsername($rootScope.globals.currentUser.username)
+                .then(function (user) {
+                    vm.user = user;
+                });
+        }
+
+        function loadAllUsers() {
+            UserService.GetAll()
+                .then(function (users) {
+                    vm.allUsers = users;
+                });
+        }
+
+        function deleteUser(id) {
+            UserService.Delete(id)
+                .then(function () {
+                    loadAllUsers();
+                });
         }
     }
 })();
--- home/home.controller.js
+++ home/home.controller.js
@@ -9,10 +9,6 @@
     function HomeController(UserService, $rootScope) {
         var vm = this;
 
-        vm.user = null;
-        vm.allUsers = [];
-        vm.deleteUser = deleteUser;
-
         initController();
 
         $rootScope.tafels = [{
@@ -122,29 +118,7 @@
         var url = window.location.protocol + "://" + window.location.host + "/" + window.location.pathname;
 
         function initController() {
-            loadCurrentUser();
-            loadAllUsers();
-        }
-
-        function loadCurrentUser() {
-            UserService.GetByUsername($rootScope.globals.currentUser.username)
-                .then(function (user) {
-                    vm.user = user;
-                });
-        }
-
-        function loadAllUsers() {
-            UserService.GetAll()
-                .then(function (users) {
-                    vm.allUsers = users;
-                });
-        }
-
-        function deleteUser(id) {
-            UserService.Delete(id)
-                .then(function () {
-                    loadAllUsers();
-                });
+            // todo init
         }
     }
 })();
