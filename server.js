ea08b42ff71c3fd4d256c191e21282eeed1e12c3
Fliermanrw <r.w.flierman@st.hanze.nl>
2017-06-22 11:09:16 +0200

    Post en get in js

--- server.js
+++ server.js
@@ -1,57 +1,43 @@
-var express   =    require("express");
-var mysql     =    require('mysql');
-var app       =    express();
+var express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
 
-var xPool      =    mysql.createPool({
-    connectionLimit : 100, //important
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+var mysql      = require('mysql');  
+var connection = mysql.createConnection({  
+  host     : 'localhost',  
+  user     : 'root',  
+  password : 'root',
+  database : 'cafepos'  
+});
+
+//  GEGEVENS VAN DE DB ONLINE
+var connectionReal = mysql.createConnection({
     host     : 'localhost',
     user     : 'u1183p12847_cafepos',
     password : 'hboict',
-    database : 'u1183p12847_cafe',
-    debug    :  false
+    database : 'u1183p12847_cafe'
 });
 
-var pool      =    mysql.createPool({
-    connectionLimit : 100, //important
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'cafepos',
-    debug    :  false
-});
+connection.connect();
 
-function handle_database(req,res) {
-    
-    pool.getConnection(function(err,connection){
-        if (err) {
-          res.json({"code" : 100, "status" : "Error in connection database"});
-          return;
-        }   
-
-        console.log('connected as id ' + connection.threadId);
-        
-        connection.query("select * from cafe",function(err,rows){
-            connection.release();
-            if(!err) {
-                res.json(rows);
-            }           
-        });
-
-        connection.on('error', function(err) {      
-              res.json({"code" : 100, "status" : "Error in connection database"});
-        });
-  });
-}
-
-app.get("/",function(req,res){-
-        handle_database(req,res);
+app.get('/api/:_id', function (req,res){
+	connection.query('select * from  `'.concat(req.params._id).concat('`'),function (err,rows,fields)
+	{
+		if (err) throw err;
+		res.json(rows);
+	});
 });
 
 app.post('/api/:_id', function (req,res){
-    var postData = req.body;
-    connection.query('insert into '.concat(req.params._id).concat(' SET ?'),postData,function(err,res){
-        if(err) throw err;
-    });
+	var postData = req.body;
+connection.query('insert into `'.concat(req.params._id).concat('` SET ?'),postData,function(err,res){
+if(err) throw err;
+});
 });
 
-app.listen(3000);
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
+});
