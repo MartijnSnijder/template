6c3a30274ca048a9ffd1c4fa9cec70006aecce38
Robert Zandberg <robert-zandberg@hotmail.com>
2017-06-28 13:03:47 +0200

    post

--- server.js
+++ server.js
@@ -4,40 +4,67 @@ const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-var mysql      = require('mysql');  
-var connection = mysql.createConnection({  
-  host     : 'localhost',  
-  user     : 'root',  
-  password : 'root',
-  database : 'cafepos'  
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'cafepos'
 });
 
 //  GEGEVENS VAN DE DB ONLINE
 var connectionReal = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'u1183p12847_cafepos',
-    password : 'hboict',
-    database : 'u1183p12847_cafe'
+    host: 'localhost',
+    user: 'u1183p12847_cafepos',
+    password: 'hboict',
+    database: 'u1183p12847_cafe'
 });
 
 connection.connect();
 
-app.get('/api/:_id', function (req,res){
-	connection.query('select * from  `'.concat(req.params._id).concat('`'),function (err,rows,fields)
-	{
-		if (err) throw err;
-		res.json(rows);
-	});
+app.get('/api/:_id', function (req, res) {
+    connection.query('select * from  `'.concat(req.params._id).concat('`'), function (err, rows, fields) {
+        if (err) throw err;
+        res.json(rows);
+    });
 });
 
-app.post('/api/:_id', function (req,res){
-	var postData = req.body;
-connection.query('insert into `'.concat(req.params._id).concat('` SET ?'),postData,function(err,res){
-if(err) throw err;
+
+
+
+app.post('/api/:_id', function (req, res) {
+    var postData = req.body,
+        query = 'INSERT INTO `'.concat(req.params._id).concat('` (');
+
+    for (var key in postData) {
+        if (!postData.hasOwnProperty(key))
+            continue;
+
+        query = query.concat('`').concat(key).concat('`,');
+    }
+
+    var data = [];
+    query = query.slice(0, -1);
+    query = query.concat(') VALUES(');
+    for (var key in postData) {
+        if (!postData.hasOwnProperty(key))
+            continue;
+
+        query = query.concat('?,');
+        data.push(postData[key]);
+    }
+
+    query = query.slice(0, -1);
+    query = query.concat(')');
+    var queryResult = connection.query(query, data, function (err, res2) {
+        if (err) throw err;
+        res.json(true);
+    });
 });
-});
+
 
 app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+    console.log('Listening on port 3000!');
 });
