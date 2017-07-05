5d570a073ec05622219bbc3009fe5e2661459a0d
Robert Zandberg <robert-zandberg@hotmail.com>
2017-07-05 09:27:35 +0200

    Product-log and user-log

--- Mongo.js
+++ Mongo.js
@@ -2,143 +2,77 @@
  * Created by Robert on 29-6-2017.
  */
 
-startMongo();
-function startMongo() {
-    var MongoClient = require('mongodb').MongoClient
-        , assert = require('assert');
-    var uri = "mongodb://project2-4:hboict@cluster0-shard-00-00-rgu4f.mongodb.net:27017,cluster0-shard-00-01-rgu4f.mongodb.net:27017,cluster0-shard-00-02-rgu4f.mongodb.net:27017/Cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
 
-    MongoClient.connect(uri, function (err, db) {
+var uri = "mongodb://project2-4:hboict@cluster0-shard-00-00-rgu4f.mongodb.net:27017,cluster0-shard-00-01-rgu4f.mongodb.net:27017,cluster0-shard-00-02-rgu4f.mongodb.net:27017/Cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+MongoClient.connect(uri, function(err, db) {
         assert.equal(null, err);
         console.log("Connected successfully to server");
-        // db.close();
-    });
-}
 
-
-//
-//
-//     function startMongo() {
-//     var MongoClient = require('mongodb').MongoClient
-//         , assert = require('assert');
-//     var uri = "mongodb://project2-4:hboict@cluster0-shard-00-00-rgu4f.mongodb.net:27017,cluster0-shard-00-01-rgu4f.mongodb.net:27017,cluster0-shard-00-02-rgu4f.mongodb.net:27017/Cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
-//
-//     MongoClient.connect(uri, function (err, db) {
-//         assert.equal(null, err);
-//         console.log("Connected successfully to server");
-//         db.close();
-//     });
-// }  var insertOneLogFile = function (db, callback)
-//
-function insertProduct(db, item) {
-    console.log("Wordt de MONGO troep hier aangeroepen ?");
-    console.log(JSON.stringify(item));
-    console.log(JSON.stringify(item.naam));
-
-    var collection = db.collection('log');
-    console.log('Leef ik nog ?');
-    collection.insertOne({
-        errorname: "MONGO HARD",
-        date: new Date(),
-        description: "Omschrijving error",
-        errorNumber: 2,
-        status: "Undefiend"
-    }, function (err, result) {
-        console.log("Inserted 1 errors into the collection");
-        callback(result);
-    });
-}
-//     //
-//     var insertOneLogFile = function (db, callback) {
-//         // Get the documents collection
-//         var collection = db.collection('product-log');
-//
-//         collection.insertOne({
-//             errorname: "MONGO HARD",
-//             date: new Date(),
-//             description: "Omschrijving error",
-//             errorNumber: 2,
-//             status: "Undefiend"
-//         }, function (err, result) {
-//             console.log("Inserted 1 errors into the collection");
-//             callback(result);
-//         });
-//     }
-//
-// //
-//
-// };
-// startMongo();
-
-
-/**
- var MongoClient = require('mongodb').MongoClient
- , assert = require('assert');
-
- var uri = "mongodb://project2-4:hboict@cluster0-shard-00-00-rgu4f.mongodb.net:27017,cluster0-shard-00-01-rgu4f.mongodb.net:27017,cluster0-shard-00-02-rgu4f.mongodb.net:27017/Cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
- MongoClient.connect(uri, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-
-    insertLogFiles(db, function() {
-        insertOneLogFile(db,function () {
-            findLogs(db, function () {
-                updateLog(db, function() {
-                    removeLog(db, function() {
-                        db.close();
-                    });
-                });
+    insertProductLog(db, function() {
+        insertOneUser(db,function () {
+            findProduct(db, function () {
+                updateProduct(db, function() {
+                 removeProduct(db, function() {
+                     db.close();
+                 });
+            });
             });
         });
-    });
+        });
 });
 
- var insertLogFiles = function(db, callback) {
-    // Definieer hier de collection log, wanneer nieuw wordt hij auto aangemaakt
-    var collection = db.collection('log');
+var insertProductLog= function(db, callback) {
+    // Definieer hier de collection log, wanneer nieuw wordt hij auto aangemaakt "id": 1,
+    var collection = db.collection('product-log');
 
     // Import meerdere values
     collection.insertMany([
-        { errorname: "Boot failure",
+        { productname: "RedBull",
+            prijs: 109,
             date: new Date(),
-            description: "Omschrijving error",
-            errorNumber: 2,
-            status: "Undefiend"},
-        { errorname: "broken screen",
+            type: "sapjes",
+            product_id: 2,
+            subcategorieen_id: 1,
+            cafe_id: 1},
+        { productname: "Ranja",
+            prijs: 50,
             date: new Date(),
-            description: "scherm stuk",
-            errorNumber: 3,
-            status: "solved"}
+            type: "sapjes",
+            product_id: 6,
+            subcategorieen_id: 1,
+            cafe_id: 1}
     ], function(err, result) { //aantal staat nu nog hard op 2 achter .equal(
         assert.equal(err, null);
         assert.equal(2, result.result.n);
         assert.equal(2, result.ops.length);
-        console.log("Inserted 2 errors into the collection");
+        console.log("Inserted 2 products in the log");
         callback(result);
     });
 }
 
- var insertOneLogFile = function(db, callback) {
+var insertOneUser = function(db, callback) {
     // Get the documents collection
-    var collection = db.collection('log');
+    var collection = db.collection('user-log');
 
     collection.insertOne({
         // _id veld wordt automatisch aangemaakt
-        errorname: "bluescreen",
+        username: "Braampje95",
         date: new Date(),
-        description: "Omschrijving error",
-        errorNumber: 1,
-        status: "Undefiend"
+        naam: "Braam Melle",
+        rechten: "cafeUser",
+        cafe_id: 1
     }, function(err, result) {
-        console.log("Inserted 1 errors into the collection");
+        console.log("Inserted 1 user into the collection");
         callback(result);
     });
 }
 
 
- var findLogs = function(db, callback) {
-    var collection = db.collection( 'log' );
-    collection.find({ 'errorname' : 'bluescreen' }).sort({ 'errorNumber': 1 }).toArray(function(err, docs) {
+var findProduct= function(db, callback) {
+    var collection = db.collection( 'product-log' );
+    collection.find({ 'product_id' : 6 }).sort({ 'cafe_id': 1 }).toArray(function(err, docs) {
         assert.equal(err, null);
         console.log("Found the following records");
         console.log(docs)
@@ -147,36 +81,35 @@ function insertProduct(db, item) {
 }
 
 
- var updateLog = function(db, callback) {
+var updateProduct= function(db, callback) {
     // Get the documents collection
-    var collection = db.collection('log');
+    var collection = db.collection('product-log');
     // Update document where status is Undefiend , set b equal to 1
-    collection.updateOne({ status : "Undefiend"}
-        , { $set: { status : "undefined" } }, function(err, result) {
+    collection.updateOne({ productname : "Ranja"}
+        , { $set: { productname : "AA-sportdrink" } }, function(err, result) {
             assert.equal(err, null);
             assert.equal(1, result.result.n);
-            console.log("Updated the document with the field status equal to undefined");
+            console.log("Updated the document with the field productname equal to Ranja");
             callback(result);
         });
 }
 
- var removeLog = function(db, callback) {
+var removeProduct = function(db, callback) {
     // Get the documents collection
-    var collection = db.collection('log');
+    var collection = db.collection('product-log');
     // Delete document where status is solved
-    collection.deleteOne({ status : "solved" }, function(err, result) {
+    collection.deleteOne({ product_id : 6 }, function(err, result) {
         assert.equal(err, null);
         assert.equal(1, result.result.n);
-        console.log("Removed the document with the field status equal to solved");
+        console.log("Removed the document with the field product_id equal to 1");
         callback(result);
     });
 }
 
 
- // var uriTestDb = "mongodb://project2-4:hboict@cluster0-shard-00-00-rgu4f.mongodb.net:27017,cluster0-shard-00-01-rgu4f.mongodb.net:27017,cluster0-shard-00-02-rgu4f.mongodb.net:27017/Cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
- // MongoClient.connect(uriTestDb, function(err, db) {
+// var uriTestDb = "mongodb://project2-4:hboict@cluster0-shard-00-00-rgu4f.mongodb.net:27017,cluster0-shard-00-01-rgu4f.mongodb.net:27017,cluster0-shard-00-02-rgu4f.mongodb.net:27017/Cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+// MongoClient.connect(uriTestDb, function(err, db) {
 //     db.close();
 // });
- //
- //
- */
+//
+//
